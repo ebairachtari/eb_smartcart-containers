0ea753da672114a3ec125bc2a580cc176dbb9925
Evgenia Bairachtari <121518003+ebairachtari@users.noreply.github.com>
2025-06-20 01:37:48 +0000

    Update DB_config.py

--- app/db/DB_config.py
+++ app/db/DB_config.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 # Σύνδεση με MongoDB
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient("mongodb://mongo:27017")
 db = client["smartcart_db"]
 
 # Όλα τα collections
